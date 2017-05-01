089936fec67d65085bb2b09a376ded98503b3419
Chelsea Weber <chelseaweber92@gmail.com>
2017-05-01 15:22:29 -0400

    basic functionaility

--- script.js
+++ script.js
@@ -1,25 +1,10 @@
-var app = angular.module('ourModule', []); //needs to be first line of script file, ourModule is the name of the module not the actaul app
+var app = angular.module('toDoModule', []); 
 
-//app.controller('ourController', function($scope){
-//	console.log('ourController has loaded');//important for when we have multiple controllers and we need to check which ones are loading correctly
-//	$scope.name = 'Beyonce';
-//	
-//	$scope.dogs = [
-//		{name: 'Munch', breed: 'demon', age: 2}, 
-//		{name: 'Lilly', breed: 'cavalier', age: 13}, 
-//		{name: 'Fred', breed: 'yorkie-mutt', age: 4}, {name: 'Chip', breed: 'wiener-mutt', age: 1}
-//	];
-//	
-//	$scope.addDog = function(newDog) {
-//		$scope.dogs.push(newDog);
-//	};
-//});
-
-app.controller('ourController', function($scope) {
+app.controller('toDoController', function($scope) {
 	$scope.list = [];
-	$scope.addItem = function(newItem) {
-		console.log(newItem);
-		$scope.list.push({name: newItem.name, cost: newItem.cost});
+	$scope.addTask = function(newTask) {
+		console.log(newTask);
+		$scope.list.push({name: newTask.name});
 		console.log($scope.list);
 	}
 });
