e1def24ef768587259764e97e88d87da87d89dae
Chelsea Weber <chelseaweber92@gmail.com>
2017-05-01 15:45:05 -0400

    tasks and appts

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 var app = angular.module('toDoModule', []); 
 
 app.controller('toDoController', function($scope) {
-	$scope.list = [];
+	$scope.apptList = [];
+	$scope.taskList = [];
+	
+	$scope.addAppt = function(newAppt) {
+		console.log(newAppt);
+		$scope.apptList.push({name: newAppt.name, time: newAppt.time});
+		console.log($scope.apptList);
+	}
+	
 	$scope.addTask = function(newTask) {
 		console.log(newTask);
-		$scope.list.push({name: newTask.name});
-		console.log($scope.list);
+		$scope.taskList.push({name: newTask.name});
+		console.log($scope.taskList);
 	}
+
 });
