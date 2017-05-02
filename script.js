ab44d5e5b5ab7869ef59b347bd5bfba9b40893a0
Chelsea Weber <chelseaweber92@gmail.com>
2017-05-01 21:21:07 -0400

    remove items

--- script.js
+++ script.js
@@ -3,15 +3,23 @@ var app = angular.module('toDoModule', []);
 app.controller('toDoController', function($scope) {
 	$scope.apptList = [];
 	$scope.taskList = [];
-	
+	//add new appointment
 	$scope.addAppt = function(newAppt) {
 		$scope.apptList.push({date: newAppt.date, name: newAppt.name, time: newAppt.time});
 		console.log($scope.apptList);
 	}
-	
+	//add new task
 	$scope.addTask = function(newTask) {
-		$scope.taskList.push({date: newTask.date, name: newTask.name});
+		$scope.taskList.push({name: newTask.name});
 		console.log($scope.taskList);
+	}
+	//removed appointment
+	$scope.removeAppt=function($index) { 
+  		$scope.apptList.splice($index,1);  
+	}
+	//remove task
+	$scope.removeTask=function($index) { 
+  		$scope.taskList.splice($index,1);  
 	}
 
 });
